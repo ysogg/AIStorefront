1d0286d053af190866ecc62d01d515cad4fdad40
ysogg <ajb.personal@hotmail.com>
2025-07-11 22:25:29 -0400

    Gemini to webscraper integration

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -1,46 +1,59 @@
 import { createElement } from "react";
-import axios from 'axios'
+import axios, { AxiosResponse } from 'axios'
+import "../Styles/global.css"
 
 interface CardProps {
   url: string;
 }
 
-
+//Need to get this all react stated and stuff to get it to render properly
+//but is making the requests fine and getting correct variables
+//ALSO filter out bad requests so you don't flood the console and confirm all cards created are from valid requests
 const Card = ({ url }: CardProps) => {
-    async function scrape() {
-        try {
-                    const response = await axios.post('http://localhost:8000/scrape', {
-                        url: url
-                    });
-                    console.log(response.data);
-                } catch (error) {
-                    console.error("Error posting data:", error);
-                }
+    var img = ""
+    var title = ""
+    var price = ""
+
+    const scrape = async () => {
+        if (url != "") {
+            try {
+                const response = await axios.post('http://localhost:8000/scrape', {
+                    url: JSON.stringify(url).slice(1,-1)
+                });
+                console.log(response.data);
+
+                img = response.data.img_url
+                title = response.data.title
+                price = response.data.price
+                
+            } catch (error) {
+                console.error("Error posting data:", error);
+                return null
+            }
+        }
     }
-//     return createElement(
-//         'div',
-//         { className: 'card',
-            
-//         },
-//         createElement('img',
-//             { className: 'card-img',
-//                 src: "img_placeholder.jpg"
-//             },
-//         ),
-//         createElement('h1',
-//             { className: 'card-header' },
-//             title
-//         ),
-//         createElement('p',
-//             { className: 'card-desc display-linebreak' },
-//             desc
-//         )
-//     );
-return(
-    <>
-        <button onClick={scrape}>SCRAPE</button>
-    </>
-)
+    
+    scrape()
+
+    return createElement(
+        'div',
+        { className: 'card',
+            onClick: () => window.location.href = url
+        },
+        createElement('img',
+            { className: 'card-img',
+                src: img
+            },
+        ),
+        createElement('h1',
+            { className: 'card-header' },
+            title
+        ),
+        createElement('p',
+            { className: 'card-price' },
+            price
+        )
+    );
 }
 
 export default Card
--- src/Components/SearchBar.tsx
+++ src/Components/SearchBar.tsx
@@ -1,6 +1,10 @@
-import React from "react";
+import { useState }from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
-import { GoogleGenAI } from "@google/genai";
+import { GoogleGenAI, Type } from "@google/genai";
+import { url } from "inspector";
+import Card  from "./Card"
+import { json } from "stream/consumers";
+import "../Styles/global.css"
 
 
 type Inputs = {
@@ -20,19 +24,12 @@ const SearchBar = () => {
         handleSubmit
     } = useForm<Inputs>();
 
-    const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data.query)
+    const [arr, setArr] = useState([""])
 
-    //Quickstart, answers a question
-    async function runQuery() {
-        const response = await ai.models.generateContent({
-            model: "gemini-2.5-flash",
-            contents: "Explain how AI works in a few words",
-        });
-        
-        return response.text;
-    }
+    // var arr: string[] = [];
 
     const search: SubmitHandler<Inputs> = async (userInput) => {
+        
         const listResponse = await ai.models.generateContent({
             model: "gemini-2.5-flash",
             contents: "Answer the following question and output in a list. " + userInput.query,
@@ -41,23 +38,42 @@ const SearchBar = () => {
         const searchResponse = await ai.models.generateContent({
             model: "gemini-2.5-flash",
             contents: [
-                "Use Google Shoppong to find me Amazon links for products that match items in this list: " + listResponse.text + ". Do not embed the links give the full url for each product." //May need to tweak but got links from this prompt
+                "Find me Amazon links for products that match items in this list: " + listResponse.text + ". Do not embed the links give the full url for each product. Produce JSON matching this specification: \
+                Products = [ url, ] \
+                Return: array<Products>" 
             ],
             config: {
-                tools: [{urlContext: {}}, {googleSearch: {}}],
+                tools: [{urlContext: {}}, {googleSearch: {}}]
             }
         });
 
         console.log(searchResponse.text)
-        console.log(searchResponse.candidates)
-    }
+        // console.log(searchResponse.candidates)
 
+       if (searchResponse.text != undefined) {
+            try {
+                setArr(JSON.parse(searchResponse.text.slice(7, -4))["Products"])
+                console.log(arr)
+            } catch (err){
+                console.log(err)
+            }
+        } else {
+            console.log("Err: Search response undefined")
+        }
+    }
+        
     return(
+        <>
         <form onSubmit={handleSubmit(search)}>
             <input defaultValue="" {...register("query")} />
-
             <input type="submit" />
         </form>
+        <div className="grid"> 
+            { arr && (
+                arr.map(e => <Card url={e}/>)
+            )}
+        </div>
+        </>
     );
 }
 
