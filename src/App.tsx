c35e6fe764e751863eee2d46dcf5538b6ddcf8f4
ysogg <ajb.personal@hotmail.com>
2025-07-17 21:17:00 -0400

    UI stuff and improved reliability on Gemini prompt

--- src/App.tsx
+++ src/App.tsx
@@ -11,15 +11,6 @@ function App() {
       <header className="App-header">
         <img src={require('./logo_white.png')}/>
         <SearchBar />
-        {/* testing */}
-        {/* <div className="grid">
-          <Card url={""} data={{img:"https://upload.wikimedia.org/wikipedia/commons/thumb/7/7f/Balantiocheilos_melanopterus_-_Karlsruhe_Zoo_02_%28cropped%29.jpg/1280px-Balantiocheilos_melanopterus_-_Karlsruhe_Zoo_02_%28cropped%29.jpg",title:"test title test title yaaaytttttttttttttttttttt",price:"20"}}/>
-          <Card url={""} data={{img:"https://upload.wikimedia.org/wikipedia/commons/thumb/7/7f/Balantiocheilos_melanopterus_-_Karlsruhe_Zoo_02_%28cropped%29.jpg/1280px-Balantiocheilos_melanopterus_-_Karlsruhe_Zoo_02_%28cropped%29.jpg",title:"test",price:"20"}}/>
-          <Card url={""} data={{img:"https://upload.wikimedia.org/wikipedia/commons/thumb/7/7f/Balantiocheilos_melanopterus_-_Karlsruhe_Zoo_02_%28cropped%29.jpg/1280px-Balantiocheilos_melanopterus_-_Karlsruhe_Zoo_02_%28cropped%29.jpg",title:"test",price:"20"}}/>
-          <Card url={""} data={{img:"https://upload.wikimedia.org/wikipedia/commons/thumb/7/7f/Balantiocheilos_melanopterus_-_Karlsruhe_Zoo_02_%28cropped%29.jpg/1280px-Balantiocheilos_melanopterus_-_Karlsruhe_Zoo_02_%28cropped%29.jpg",title:"test",price:"20"}}/>
-          <Card url={""} data={{img:"https://upload.wikimedia.org/wikipedia/commons/thumb/7/7f/Balantiocheilos_melanopterus_-_Karlsruhe_Zoo_02_%28cropped%29.jpg/1280px-Balantiocheilos_melanopterus_-_Karlsruhe_Zoo_02_%28cropped%29.jpg",title:"test",price:"20"}}/>
-        </div> */}
-        
       </header>
       
     </div>
--- src/Components/SearchBar.tsx
+++ src/Components/SearchBar.tsx
@@ -6,6 +6,7 @@ import Card  from "./Card"
 import "../Styles/global.css"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { ThreeDot } from "react-loading-indicators";
 
 type Inputs = {
     query: string
@@ -36,6 +37,7 @@ const SearchBar = () => {
     const [renderCards, setRenderCards] = useState(false)
     const [isInitialMount, setIsInitialMount] = useState(true)
     const [list, setList] = useState<Data>({"":{img:"",title:"",price:""}})
+    const [isLoading, setIsLoading] = useState(false)
 
     useEffect(() => {
         if (!isInitialMount) {
@@ -49,6 +51,7 @@ const SearchBar = () => {
                     }
                 }
                 setRenderCards(true)
+                setIsLoading(false)
             }
 
             if (!renderCards) {
@@ -61,6 +64,8 @@ const SearchBar = () => {
 
 
     const search: SubmitHandler<Inputs> = async (userInput) => {
+        setList({"":{img:"",title:"",price:""}})
+        setIsLoading(true)
         const listResponse = await ai.models.generateContent({
             model: "gemini-2.5-flash",
             contents: "Answer the following question and output in a list. " + userInput.query,
@@ -69,7 +74,7 @@ const SearchBar = () => {
         const searchResponse = await ai.models.generateContent({
             model: "gemini-2.5-flash",
             contents: [
-                "Find me Amazon links for products that match items in this list: " + listResponse.text + ". Do not embed the links give the full url for each product. Produce JSON matching this specification: \
+                "Find me Amazon links for products that match items in this list: " + listResponse.text + ". Do not embed the links give the full url for each product. Only produce JSON matching this specification: \
                 Products = [ url, ] \
                 Return: array<Products>" 
             ],
@@ -87,7 +92,9 @@ const SearchBar = () => {
                 setArr(JSON.parse(searchResponse.text.slice(7, -4))["Products"])
                 console.log(arr)
             } catch (err){
-                //todo gemini messed up formatting so try and re-run the search function if possible
+                //If you reached here then Gemini likely messed up the JSON formatting so try and re-run the search function
+                //Would also need to implement something to stop after a while in case of infinite loop
+                // await search(userInput)
                 console.log(err)
             }
         } else {
@@ -116,11 +123,21 @@ const SearchBar = () => {
     return(
         <>
         <form className="searchbar" onSubmit={handleSubmit(search)}>
-            <input defaultValue="" {...register("query")} />
-            <button type="submit">
-            {<FontAwesomeIcon icon={faMagnifyingGlass} />}
-            </button>
+            <div className="searchGrid">
+                <input defaultValue="" {...register("query")} />
+                <button className="searchBtn" type="submit">
+                    <span><FontAwesomeIcon icon={faMagnifyingGlass} /></span>
+                </button>
+            </div>
         </form>
+
+        { isLoading && (
+            <div className="loadState">
+                <p>(This may take a few minutes)</p>
+                <ThreeDot color="#aea9ddff" size="medium" text="" textColor="" />
+            </div>
+        )}
+        
         <div className="grid">
             { renderCards && (
                 arr.filter(e => list[e]).map(url => <Card url={url} data={list[url]}/>)
